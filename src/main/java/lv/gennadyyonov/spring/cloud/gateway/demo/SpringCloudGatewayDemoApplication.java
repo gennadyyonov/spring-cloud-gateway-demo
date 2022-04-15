4543e4abf7b7c29821b10cd11f88946a193123d6
Gennady Yonov <g.jonovs@mail.ru>
2022-04-15 21:54:20 +0300

    -- Spring Cloud Gateway Demo Application

---
+++ src/main/java/lv/gennadyyonov/spring/cloud/gateway/demo/SpringCloudGatewayDemoApplication.java
@@ -0,0 +1,12 @@
+package lv.gennadyyonov.spring.cloud.gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/lv/gennadyyonov/spring/cloud/gateway/demo/SpringCloudGatewayDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package lv.gennadyyonov.spring.cloud.gateway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
